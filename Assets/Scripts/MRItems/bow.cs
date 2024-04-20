c2b13af5a530392e68773ccc662591cc1bea2e75
gaoxiangluke <gaoxiangluke@gmail.com>
2024-04-20 12:47:13 -0400

    Working pulling

--- Assets/Scripts/MRItems/bow.cs
+++ Assets/Scripts/MRItems/bow.cs
@@ -1,23 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using Meta.WitAi.Attributes;
 using Oculus.Interaction;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class bow : MonoBehaviour
 {
+    [Header("Bow Settings")]
     [SerializeField] private Transform startPosition;
     [SerializeField] private Transform endPosition;
     [SerializeField] private Transform stringPosition;
     [SerializeField] private LineRenderer _stringRenderer;
+
+    [Header("Interaction Settings")] [SerializeField]
+    private OneGrabTranslateTransformer _stringConstraint;
+    [SerializeField] private SnapInteractable _bowSnapInteractable;
+    [SerializeField] private GrabInteractable _stringGrabInteractable;
+    private GameObject _arrow;
+    private bool _isArrowAttached = false;
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Test xxxxxxx");
+        _bowSnapInteractable.WhenSelectingInteractorAdded.Action += HandleArrowAttached;
+        _stringGrabInteractable.WhenInteractorRemoved.Action += HandleArrowReleased;
+        //Update string constraint 
+        FloatConstraint stringConstraint = new FloatConstraint();
+        stringConstraint.Constrain = true;
+        stringConstraint.Value = endPosition.position.z - startPosition.position.z;
+        Debug.Log("string value " + stringConstraint.Value);
+        // _stringConstraint.Constraints.MaxZ = stringConstraint;
+    }
+
+    private void HandleArrowReleased(GrabInteractor obj)
+    {
+        if (!_isArrowAttached)
+        {
+            return;
+        }
         
+        _isArrowAttached = false;
+        Debug.Log("Arrow Released");
+        
+    }
+
+    private void HandleArrowAttached(SnapInteractor obj)
+    {
+        Debug.Log("Arrow Attached");
+        _isArrowAttached = true;
+        // traverse the arrowsocket interactable to get the interactor 
+        foreach (var snapInteractor in _bowSnapInteractable.SelectingInteractors)
+        {
+            Debug.Log("Snap Interactor " + snapInteractor.gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        float pullingDistance = Vector3.Distance(stringPosition.position, startPosition.position);
+        if (pullingDistance > 0.01f)
+        {
+            ReleaseString();
+        }
         RenerString();
     }
 
@@ -28,4 +74,10 @@ public class bow : MonoBehaviour
         _stringRenderer.SetPosition(1,newStringPosition );
             
     }
+
+    private void ReleaseString()
+    {
+        stringPosition.position = Vector3.Lerp(stringPosition.position, startPosition.position, Time.deltaTime*5f);
+    }
+    
 }
