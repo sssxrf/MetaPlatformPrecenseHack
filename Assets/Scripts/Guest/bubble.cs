f18b0bd75c84e1b27f76d34d87a71b5c35e2137c
gaoxiangluke <gaoxiangluke@gmail.com>
2024-04-30 23:48:16 -0400

    add basic guest

---
+++ Assets/Scripts/Guest/bubble.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bubble : MonoBehaviour
+{
+
+    [SerializeField] private GameObject bubbleEffect;
+    [SerializeField] private Sprite rendererSprite;
+    public Food.FoodType foodType;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
