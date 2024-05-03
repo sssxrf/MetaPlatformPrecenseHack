9855132e0b74a709979dc8ec46512643c22094f6
sssxrf <100052475+sssxrf@users.noreply.github.com>
2024-05-02 20:21:00 -0400

    add a guest manager for guest infos

--- Assets/Scripts/Guest/GuestSpawn.cs
+++ Assets/Scripts/Guest/GuestSpawn.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GuestSpa : MonoBehaviour
+public class GuestSpawn : MonoBehaviour
 {
     [SerializeField] private List<GameObject> guestTypes;
     [SerializeField] private Transform spawnPoint;
